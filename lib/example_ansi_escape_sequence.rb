7005697dd53df39e6bdcd96e331a4d5b549c8533
nax2uk <azlinayeo@gmail.com>
2020-09-25 11:22:29 +0100

    edited color formatting

--- lib/example_ansi_escape_sequence.rb
+++ lib/example_ansi_escape_sequence.rb
@@ -1,10 +1,13 @@
+# Shows how to use raw ANSI excape sequences
 puts ""
-print "\e[0;34;49m     This is blue and the next line is not reset to default  \n\n" #no reset code at the end
+print "\e[0;34m    This is blue and the next line is not reset to default  \n\n" #no reset code at the end
 print "     This is the next line.\n\n"
-print "\e[0;31;49m     This is red and the next line is reset to default  \e[0m\n\n"
-print "\e[1;39;49m     This is bold font  \e[0m\n\n"
-print "\e[5;39;49m     This is a blinking font  \e[0m\n\n"
+print "\e[0;31m    This is red and the next line is reset to default  \e[0m\n\n"
 print "     This is the next line.\n\n"
+print "\e[1;32m     This is a bold green font  \e[0m\n\n"
+print "\e[5;37m     This is a blinking, white font  \e[0m\n\n"
+# Make sure that text-blinking is checked under your terminal Profiles.
+
 print "\e[0;33;45m     This is a yellow font with a magenta background  \e[0m\n\n"
 
 print "\e[38;5;153m     This is a lavendar foreground color\n\n"
